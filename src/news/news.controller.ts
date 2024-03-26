0db993ca6443f729df166797903d369ced855902
genaggg <mr.genag@mail.ru>
2024-03-26 20:41:06 +0500

    First commit

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { News, NewsService } from './news.service';
 
 @Controller('news')
 export class NewsController {
-    @Get('/:id')
-    getNews(@Param('id')id:number):string{
-        return `<h2 style="color: green">Новость №${id}</h2>`
-    }
+  constructor(private readonly newsService: NewsService) {}
+
+  @Get('/:id')
+  get(@Param('id') id: string): News {
+    let idInt = parseInt(id);
+    return this.newsService.find(idInt);
+  }
+
+  @Post()
+  create(@Body() news: News) {
+    return this.newsService.create(news);
+  }
 }
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,6 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
+export interface News {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  countView?: number;
+}
 @Injectable()
 export class NewsService {
-    
+  private readonly news: News[] = [
+    {
+      id: 1,
+      title: 'Новость про котов',
+      description: 'Коты милые',
+      author: 'GG',
+      countView: 7,
+    },
+  ];
+
+  create(news: News): News |undefined {
+    this.news.push(news);
+    return news;
+  }
+
+  find(id: News['id']): News | undefined {
+    return this.news.find((news) => {
+      return news.id === id;
+    });
+  }
 }
