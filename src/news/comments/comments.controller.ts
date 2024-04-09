4a4fab4a4c1c3c54f0752260f78fe1aebaa3f354
genaggg <mr.genag@mail.ru>
2024-04-09 19:07:03 +0500

    Создал метод удаления комментария

--- src/news/comments/comments.controller.ts
+++ src/news/comments/comments.controller.ts
@@ -1,21 +1,31 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { Comment } from './comments.service';
 
 @Controller('comments')
 export class CommentsController {
-    constructor(private readonly CommentsService: CommentsService){}
+  constructor(private readonly CommentsService: CommentsService) {}
 
-    @Post('/:idNews')
-    create(@Param('idNews') idNews: string, @Body() comment: Comment){
-        const idNewsInt = parseInt(idNews)
-       return this.CommentsService.create(idNewsInt, comment)
-    }
+  @Post('/:idNews')
+  create(@Param('idNews') idNews: string, @Body() comment: Comment) {
+    const idNewsInt = parseInt(idNews);
+    return this.CommentsService.create(idNewsInt, comment);
+  }
+
+  @Get('/details/:idNews')
+  find(@Param('idNews') idNews: string) {
+    const idNewsInt = parseInt(idNews);
+    return this.CommentsService.find(idNewsInt);
+  }
+
+  @Delete('/details/:idNews/:idComments')
+  remove(
+    @Param('idNews') idNews: string,
+    @Param('idComments') idComments: string,
+  ) {
+    const idNewsInt = parseInt(idNews);
+    const idCommentsInt = parseInt(idComments);
+    return this.CommentsService.remove(idNewsInt, idCommentsInt);
     
-    
-    @Get('/:idNews')
-    find(@Param("idNews") idNews: string){
-        const idNewsInt = parseInt(idNews)
-        return this.CommentsService.find(idNewsInt)
-    }
+  }
 }
--- src/news/comments/comments.service.ts
+++ src/news/comments/comments.service.ts
@@ -25,4 +25,20 @@ export class CommentsService {
   find(idNews: number): Comment[] | null {
     return this.comments[idNews] || null;
   }
+
+  remove(idNews: number, idComments: number) {
+    if (!this.comments[idNews]) {
+      return null;
+    }
+
+    const indexComment = this.comments[idNews].findIndex(
+      (c) => c.id === idComments,
+    );
+    if (indexComment === -1) {
+      return null;
+    }
+
+    this.comments[idNews].splice(indexComment, 1);
+    return 'Коментарий был удалён';
+  }
 }
